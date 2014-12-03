8bf5e51a1b09bddbad02b15be6fce5a16b1fdf5f
knupur <nupurk15@gmail.com>
2014-12-02 20:56:49 -0500

    Adding navigation and some erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
-  get "about" => "pages#about"
+  get "about" => "pages#about" #this generates about_path variable
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
