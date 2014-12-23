ac9373fb4b6ab79b589551fdc238b7003735b286
knupur <nupurk15@gmail.com>
2014-12-22 22:40:32 -0500

    Modifying image sizes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :medium => "600x600>", :thumb => "300x300>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates :image, presence: true
   validates :description, presence: true
